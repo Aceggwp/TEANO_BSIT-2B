0fe772d363d0afce36d68c7574c9a5468188c316
richard gwapo <richardgarganta920@gmail.com>
2026-02-23 19:12:47 +0800

    DELETE FUNCTION IN PARENT HAS BEEN ADDED By:GARGANTA

--- app/Controllers/Parents.php
+++ app/Controllers/Parents.php
@@ -70,6 +70,25 @@ class Parents extends Controller
             ]);
         }
     }
+    
+public function delete($id){
+    $model = new ParentModel();
+    $logModel = new LogModel();
+    $user = $model->find($id);
+    if (!$user) {
+        return $this->response->setJSON(['success' => false, 'message' => 'User not found.']);
+    }
+
+    $deleted = $model->delete($id);
+
+    if ($deleted) {
+        $logModel->addLog('Delete user', 'DELETED');
+        return $this->response->setJSON(['success' => true, 'message' => 'User deleted successfully.']);
+    } else {
+        return $this->response->setJSON(['success' => false, 'message' => 'Failed to delete user.']);
+    }
+}
+    
 
     public function fetchRecords() {
         $request = service('request');
